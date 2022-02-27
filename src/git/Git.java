4e79c3ec9139311a05c14406f5166a71b8f1641b
Karthik Raman <karthikraman@Karthiks-MacBook-Air.local>
2022-02-27 16:12:37 +0530

    Folder structure

---
+++ src/git/Git.java
@@ -0,0 +1,18 @@
+package git;
+
+public class Git {
+	
+	private static void git() {
+		
+		System.out.println("sample git");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is git");
+		
+		git();
+		
+	}
+	
+}
