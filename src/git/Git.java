47f3520f1eecbd46619e94873e4291a08290235a
Karthik Raman <karthikraman@Karthiks-MacBook-Air.local>
2022-02-27 21:28:30 +0530

    matched clone

--- src/git/Git.java
+++ src/git/Git.java
@@ -3,11 +3,14 @@ package git;
 public class Git {
 	
 	
-private static void git2() {
 
-		
+	
+	
+	private static void git2() {
+
 		System.out.println("sample git");
 	}
+	
 	
 	public static void main(String[] args) {
 		
