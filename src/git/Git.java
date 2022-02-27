5873542769736e35425d925e61c1ff8587b5e686
Karthik Raman <karthikraman@Karthiks-MacBook-Air.local>
2022-02-27 20:23:07 +0530

    git commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -6,11 +6,8 @@ public class Git {
 		System.out.println("sample git");
 	}
 	
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("this is git");
-		
 		git3();
 		
 		
