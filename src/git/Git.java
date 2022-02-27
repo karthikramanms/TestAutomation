c57e0ba37243ec87637099023f409212defd61a8
Karthik Raman <karthikraman@Karthiks-MacBook-Air.local>
2022-02-27 20:57:37 +0530

    final push

--- src/git/Git.java
+++ src/git/Git.java
@@ -2,16 +2,24 @@ package git;
 
 public class Git {
 	
-	private static void git3() {
+	
+private static void git2() {
+
+		
 		System.out.println("sample git");
 	}
 	
-	public static void main(String[] args) {	
-		System.out.println("this is git");
-		git3();
+	public static void main(String[] args) {
 		
+		System.out.println("this is git");
+		
+
+		git2();
+
+		
+	}	
 		
 		
 	}
 	
-}
+
