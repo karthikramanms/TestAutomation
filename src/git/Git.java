c7df57738ed2dd3161e3a62da97450598fc930a2
Karthik Raman <karthikraman@Karthiks-MacBook-Air.local>
2022-02-27 21:47:46 +0530

    cloned1

--- src/git/Git.java
+++ src/git/Git.java
@@ -6,33 +6,13 @@ public class Git {
 
 
 	
+	private static void git2() { System.out.println("sample git");}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	private static void git2() {
-
-		System.out.println("sample git");
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		System.out.println("this is git");
-		
-
-		git2();
-
-		
-	}	
 		
 		
-	}
 	
 
+
+}
